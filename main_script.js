eec0428eb4cf1a95c388f31b25565c2be2bfdb7b
gayleyii <150993314+gayleyii@users.noreply.github.com>
2024-02-25 05:27:44 +0800

    Delete main_script.js

--- main_script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("DOMContentLoaded event fired");
-
-    var backGlitch = document.querySelector('.back-glitch');
-    backGlitch.classList.add('show');
-    console.log("Before time");
-});
-
