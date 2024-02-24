76d59b27fbe1fda2715d1578983830c23dd92378
gayleyii <150993314+gayleyii@users.noreply.github.com>
2024-02-25 05:22:18 +0800

    Add files via upload

---
+++ main_script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOMContentLoaded event fired");
+
+    var backGlitch = document.querySelector('.back-glitch');
+    backGlitch.classList.add('show');
+    console.log("Before time");
+});
+
